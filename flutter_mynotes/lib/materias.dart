2287a42ef5ac7fb3c0c84588b14edb773742f466
Sebas_dev <sebastianguerrero2006.24@gmail.com>
2025-10-27 11:14:51 -0500

    moficacion prueba

--- flutter_mynotes/lib/materias.dart
+++ flutter_mynotes/lib/materias.dart
@@ -24,3 +24,64 @@ class _MaterialScreenState extends State<MaterialScreen> {
     });
   });
 }
+
+@override
+ Widget build(BuildContext context) {
+  return ListenableBuilder(
+    listenable: _themeProvider,
+    builder: (context, child) {
+      return Scaffold(
+        backgroundColor: _themeProvider.backgroundColor,
+        appBar: AppBar(
+          backgroundColor: _themeProvider.primaryColor,
+          elevation: 0,
+          title: Row(
+            children: [
+              const Text(
+                'MyNote. ',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              Icon(
+                _themeProvider.isDarkTheme ? Icons.nights_stay : Icons.wb_sunny,
+                color: Colors.yellow,
+                size: 24,
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(
+                _themeProvider.isDarkTheme ? Icons.wb_sunny : Icons.nights_stay,
+                color: Colors.white,
+              ),
+              onPressed: (){
+                _themeProvider.toggleTheme();
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const edgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Materias',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: _themeProvider.textColor,
+                ),
+              )
+            ]
+          )
+        ),
+      );
+    }
+  )
+}
+}

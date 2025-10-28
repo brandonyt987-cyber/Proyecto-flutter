803bafa9e63259790204701d1a539b462ea9f806
brandon <brandonyt987@gmail.com>
2025-10-27 22:03:46 -0500

    Ya casi terminooooo xd

--- flutter_mynotes/lib/main.dart
+++ flutter_mynotes/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(
+    MaterialApp(home: MainApp()),
+  ); //El home mainapp tiene el contenido principal
 }
 
 class MainApp extends StatelessWidget {
@@ -14,31 +16,68 @@ class MainApp extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment
-              .start, //Se hace una are para el codigo y se alinea a la izquierda
+        child: Stack(
+          //stack permite poner un widget sobre
           children: [
-            /* Align(
-              alignment: Alignment.topRight,
-              child: Image.asset(
-                'assets/images/lapiz.png',
-                height: 80, //Tamaño del lapiz
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment
+                    .center, //Sirve para central horizontalmentey el otro vertical mente
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20), // Espacio
+                  Text(
+                    'MyNote.',
+                    style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                      color: textColor,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Bienvenido',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                      color: textColor,
+                    ),
+                  ),
+                ],
               ),
             ),
+            //Imagen
+            Positioned(
+              top: 20,
+              right: 20, //Se usa para poner la imagen en la esquina
+              child: Image.asset('assets/images/lapiz.png', height: 80),
+            ),
 
-            */
-            const SizedBox(height: 20), //Espacio del lapioz
-            //MyNote
-            Text(
-              'MyNote.',
-              style: TextStyle(
-                fontSize: 34,
-                fontWeight: FontWeight.bold,
-                color: textColor,
+            //icono de home para la otra pagina
+            Container(
+              padding: const EdgeInsets.all(20), //espacio
+              decoration: BoxDecoration(
+                shape: BoxShape.circle, //Fondo redondo
+                color: Colors.grey[300], //color del circulo
               ),
+              //Icono
+              child: const Icon(Icons.home, size: 40, color: Colors.black54),
             ),
           ],
         ),
+      ),
+      //barra del final xd
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey,
+        currentIndex: 0, // Primer
+
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inicio'),
+          BottomNavigationBarItem(icon: Icon(Icons.note), label: 'Notas'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Ajustes'),
+        ],
       ),
     );
   }

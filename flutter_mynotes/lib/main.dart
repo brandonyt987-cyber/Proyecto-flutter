eff3764ac536ab55104978d0533dc81309ec33cc
Sebas_dev <sebastianguerrero2006.24@gmail.com>
2025-11-03 22:06:49 -0500

    Cambio

--- flutter_mynotes/lib/main.dart
+++ flutter_mynotes/lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   // 🔥 SOLO PARA DESARROLLO: Borra la BD al iniciar
-  // await DatabaseService.instance.deleteDatabase();
+  await DatabaseService.instance.deleteDatabase();
   
   runApp(const MyNoteApp());
 }
